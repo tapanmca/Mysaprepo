03ea6c93ba0a597722a354f628e4979619ecd25e
kumar tapan <tapanmca2011@gmail.com>
2018-01-08 03:09:16 +0530

    second commit

--- src/com/def/demo/ForTest.java
+++ src/com/def/demo/ForTest.java
@@ -6,6 +6,8 @@ public class ForTest {
 		/*char[] ASD={'A','B','C','D','E'};
 		// TODO Auto-generated meta stub
 		//Tapan kumar Rout 123@345
+		 //secndcomit of forchk
+		 
 		for(int i=1;i<=5;i++ )
 		{
 
